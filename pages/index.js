da430d587fdd07c9bf1c1dcaf659b2d8d8d412b9
rahmagillan <rahmagillan2001@gmail.com>
2021-12-16 20:36:17 -0500

    initialize fb rtdb

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import firebaseConfig from '../utils/firebaseConfig'
+import { getDatabase, ref, set } from "firebase/database"
+import { initializeApp } from '@firebase/app'
 
 export default function Home() {
+  const app = initializeApp(firebaseConfig)
+  const db = getDatabase(app)
+  
   return (
     <div className={styles.container}>
       <Head>
---
+++ utils/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
+  databaseURL: process.env.NEXT_PUBLIC_databaseURL
+}
+
+export default firebaseConfig
