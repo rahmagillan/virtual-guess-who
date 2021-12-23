7279129a8e9897ef67dcbfe6c4a291d074424a9a
rahmagillan <rahmagillan2001@gmail.com>
2021-12-22 22:38:18 -0500

    question feed nonsocket db cnnction

--- components/questionFeed.js
+++ components/questionFeed.js
@@ -14,36 +14,8 @@ import Button from '@mui/material/Button'
 import { ref, push, set, child, onValue, update, orderByChild, query } from 'firebase/database'
 
 function SubmitQuestion(props) {
+    const {db, setRefresh} = props
     const [question, setQuestion] = useState('')
-    const [submitted, setSubmitted] = useState(false)
-    const db = props.db
-    const setQuestions = props.setQuestions
-
-    useEffect(() => {
-        const test = query(ref(db, 'questions/'), orderByChild('upvotes'))
-        const fetchQuestions = []
-        const keys = []
-        const questions = []
-        onValue(test,  (snapshot) => {
-            snapshot.forEach((child) => {
-                keys.push(child.key)
-                fetchQuestions.push(child.val())
-            })
-        })
-
-        fetchQuestions.reverse()
-        keys.reverse()
-
-        for (let q in fetchQuestions) {
-            questions.push({
-                "key": keys[q],
-                "data": fetchQuestions[q]
-            })
-        }
-        
-        setQuestions(questions)
-
-    }, [db, submitted, setQuestions]) 
      
     const handleChange = (event) => {
         setQuestion(event.target.value)
@@ -57,7 +29,7 @@ function SubmitQuestion(props) {
             upvotes: 0,
             downvotes: 0
         })
-        setSubmitted(true)
+        setRefresh(true)
         setQuestion("")
     }
 
@@ -93,17 +65,7 @@ function Question(props) {
     const upvoteColor = blue[500]
     const downvoteColor = purple[500]
     const rowData = data.questions[index]
-    const [upvotes, setUpvotes] = useState(rowData.data.upvotes)
-    const [downvotes, setDownvotes] = useState(rowData.data.downvotes)
     const vote = data.voteOnClick
-
-    useEffect(() => {
-        vote('up', index)
-    }, [vote, upvotes, index])
-    
-    useEffect(() => {
-        vote('down', index)
-    }, [vote, downvotes, index])
 
     return (
         <ListItem style={style} key={index} component="div" disablePadding>
@@ -112,17 +74,43 @@ function Question(props) {
                 secondary={rowData.data.author ? rowData.data.author : "No Author"}
             />
             <Votes votes={upvotes} color={upvoteColor} />
-            <IconButton onClick={() => setUpvotes(++upvotes)} color="primary"><KeyboardArrowUpIcon /></IconButton>
+            <IconButton onClick={() => vote('up', index)} color="primary"><KeyboardArrowUpIcon /></IconButton>
             <Votes votes={downvotes} color={downvoteColor} />
-            <IconButton onClick={() => setDownvotes(++downvotes)} color="secondary"><KeyboardArrowDownIcon /></IconButton>
+            <IconButton onClick={() => vote('down', index)} color="secondary"><KeyboardArrowDownIcon /></IconButton>
         </ListItem>
     )
 }
 
 export default function QuestionFeed(props) {
+    const { db } = props
+    const [questions, setQuestions] = useState([])
+    const [refresh, setRefresh] = useState(false)
 
-    const [questions, setQuestions] = useState({})
-    const db = props.db
+    useEffect(() => {
+        const test = query(ref(db, 'questions/'), orderByChild('upvotes'))
+        const fetchQuestions = []
+        const keys = []
+        const questions = []
+        onValue(test,  (snapshot) => {
+            snapshot.forEach((child) => {
+                keys.push(child.key)
+                fetchQuestions.push(child.val())
+            })
+        })
+
+        fetchQuestions.reverse()
+        keys.reverse()
+
+        for (let q in fetchQuestions) {
+            questions.push({
+                "key": keys[q],
+                "data": fetchQuestions[q]
+            })
+        }
+        
+        setQuestions(questions)
+
+    }, [db, refresh, setQuestions]) 
 
     const vote = (type, index) => { // type 'up' or 'down'
         const question = questions[index]
@@ -130,18 +118,18 @@ export default function QuestionFeed(props) {
         const updates = {}
         updates['questions/'+question.key] = question.data
         update(ref(db), updates)
+        setRefresh(true)
     }
 
     return (
         <Box 
             sx={{ width: '100%', height: 400, maxWidth: 360 }}
         >
-            {questions[0] ? questions[0].key : null}
             <FixedSizeList
                 height={400}
                 width={360}
                 itemSize={80}
-                itemCount={questions.length}
+                itemCount={questions.length ? questions.length : 0}
                 overscanCount={5}
                 itemData={{
                     "questions": questions, 
@@ -150,7 +138,7 @@ export default function QuestionFeed(props) {
             >
                 {Question}
             </FixedSizeList>
-            <SubmitQuestion db={props.db} setQuestions={setQuestions} />
+            <SubmitQuestion db={props.db} setQuestions={setQuestions} refresh={refresh} setRefresh={setRefresh} />
         </Box>
     )
 }
